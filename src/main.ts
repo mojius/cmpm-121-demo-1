b588da7b4a56491a0913e6d72136f9744fd4749c
Benjamin Daly <bfdaly@ucsc.edu>
2023-10-08 13:54:31 -0700

    Added button and (unused) emoji font

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Ben's Game";
+const gameName = "Noisemaker";
 
 document.title = gameName;
 
-const header = document.createElement("h1");
+const header: HTMLHeadingElement = document.createElement("h1");
 header.innerHTML = gameName;
+const subHeader: HTMLHeadingElement = document.createElement("h3");
+subHeader.innerHTML = "<i>Don't worry, it's a silent game.</i>";
 app.append(header);
+app.append(subHeader);
+
+const mainButton: HTMLButtonElement = document.createElement("button"); // Look at me, all fancy with my typing!
+mainButton.className = "mainButton";
+mainButton.type = "button";
+mainButton.className = "buttonStyle";
+mainButton.textContent = "🔊⬆";
+
+app.append(mainButton);
