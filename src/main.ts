bfe9db86020ef319bbcbea7a2b0361df74128bbf
Benjamin Daly <bfdaly@ucsc.edu>
2023-10-11 12:49:50 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 let masterCounter: number = 0;
 
-const decibelsPerSecond: number = 1;
-const baseClickIncrease: number = 0.01;
+let decibelsPerSecond: number = 1;
+const baseClickIncrease: number = 1;
+let currentUpgradeCost: number = 10;
+let speakerCount: number = 0;
 
 const gameName = "Noisemaker";
 
@@ -13,9 +15,12 @@ document.title = gameName;
 
 const header: HTMLHeadingElement = document.createElement("h1");
 header.innerHTML = gameName;
+const speakerCountText: HTMLHeadingElement = document.createElement("h2");
+speakerCountText.textContent = `Speakers: ${speakerCount}`;
 const subHeader: HTMLHeadingElement = document.createElement("h3");
 subHeader.innerHTML = "<i>Don't worry, it's a silent game.</i>";
 app.append(header);
+app.append(speakerCountText);
 app.append(subHeader);
 
 const mainButton: HTMLButtonElement = document.createElement("button"); // Look at me, all fancy with my typing!
@@ -24,18 +29,47 @@ mainButton.type = "button";
 mainButton.className = "buttonStyle";
 mainButton.textContent = "🔊⬆";
 
+const upgradeButton: HTMLButtonElement = document.createElement("button");
+upgradeButton.type = "button";
+upgradeButton.disabled = true;
+upgradeButton.textContent = `Buy a speaker: (${currentUpgradeCost}) decibels`;
+
 const decibelLevel: HTMLDivElement = document.createElement("div");
 decibelLevel.textContent = getDecibelText();
 
-mainButton.addEventListener("click", onClicked);
+mainButton.addEventListener("click", onMainClicked);
+
+upgradeButton.addEventListener("click", onUpgradeClicked);
 
 app.append(mainButton);
 
 app.append(decibelLevel);
 
+app.append(upgradeButton);
+
 startAutoDecibelIncrease();
 
-function onClicked() {
+// Update loop
+setInterval(() => {
+  if (masterCounter >= currentUpgradeCost) {
+    upgradeButton.disabled = false;
+  } else {
+    upgradeButton.disabled = true;
+  }
+}, 5);
+
+function onUpgradeClicked() {
+  if (masterCounter >= currentUpgradeCost) {
+    speakerCount += 1;
+    masterCounter -= currentUpgradeCost;
+    decibelsPerSecond += 1;
+    currentUpgradeCost *= 1.2;
+    upgradeButton.textContent = `Buy a speaker: (${currentUpgradeCost}) decibels`;
+    speakerCountText.textContent = `Speakers: ${speakerCount}`;
+  }
+}
+
+function onMainClicked() {
   masterCounter += baseClickIncrease;
   decibelLevel.textContent = getDecibelText();
 }
